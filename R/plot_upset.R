2416f181bc6bd611673f0d1c3ab823b99b9c50ef
christensensm <45671925+christensensm@users.noreply.github.com>
2019-06-24 12:16:01 -0400

    Add files via upload

--- R/plot_upset.R
+++ R/plot_upset.R
@@ -21,8 +21,8 @@ plot_upset <- function(genescore, save = FALSE, cutoff = 1, list.return = FALSE)
   #Positively enriched genes (score >= cutoff)
   pos.enrich.upset = data.frame(Gene = rownames(genescore), genescore)
   pos.enrich.upset[,-1] = as.numeric(as.logical(pos.enrich.upset[,-1]>=cutoff))
-  UpSetR::upset(pos.enrich.upset, order.by = 'freq', nsets = ncol(pos.enrich.upset),
-                empty.intersections = 'on')
+  print(UpSetR::upset(pos.enrich.upset, order.by = 'freq', nsets = ncol(pos.enrich.upset),
+                empty.intersections = 'on'))
   if(save){
     grDevices::dev.copy(pdf,paste0('upset_plot_enriched_genes.pdf'),height = 8, width = 8)
     grDevices::dev.off()
@@ -52,8 +52,8 @@ plot_upset <- function(genescore, save = FALSE, cutoff = 1, list.return = FALSE)
   #Negative enriched genes (score <= -1)
   neg.enrich.upset = data.frame(Gene = rownames(genescore), genescore)
   neg.enrich.upset[,-1] = as.numeric(as.logical(neg.enrich.upset[,-1]<=-cutoff))
-  UpSetR::upset(neg.enrich.upset, order.by = 'freq', nsets = ncol(neg.enrich.upset),
-                empty.intersections = 'on')
+  print(UpSetR::upset(neg.enrich.upset, order.by = 'freq', nsets = ncol(neg.enrich.upset),
+                empty.intersections = 'on'))
   if(save){
     grDevices::dev.copy(pdf,paste0('upset_plot_depleted_genes.pdf'),height = 8, width = 8)
     grDevices::dev.off()
